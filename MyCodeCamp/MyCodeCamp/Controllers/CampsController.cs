1c3b877f67e0825d01007548a81492f7ba80d939
mahedee <mahedee.hasan@gmail.com>
2019-01-16 17:27:57 +0600

    modified on 16-01-19

---
+++ MyCodeCamp/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        //[HttpGet("api/camps")]
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Mahedee", FavouriteColor = "Violet" });
+        }
+    }
+}
